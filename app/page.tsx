4b0c2e86c32153771a8187aceea0d532f5f3d641
matthewadeboyejr <matthewconsult@gmail.com>
2025-12-28 19:56:36 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <QuickLinks />
       <HomeServices />
       <OverView />
-      <Offer />
+      {/* <Offer /> */}
       <OurDoctors />
       <Testimonies />
     </div>
