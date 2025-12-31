fa66426081bfa42091ee7c51443e2d3c0f8cb5be
matthewadeboyejr <matthewconsult@gmail.com>
2025-12-31 15:26:25 +0100

    placeholder image

--- components /teams/OurTeam.tsx
+++ components /teams/OurTeam.tsx
@@ -15,24 +15,21 @@ const OurTeam = () => {
       name: "Dr. Bolarinwa Akinola",
       title: "Visiting Consultant Orthopaedic and Arthroplasty Surgeon",
       qualifications: "FRCS (Orth/Trauma), MPH, MBSBS",
-      image:
-        "https://images.unsplash.com/photo-1673295716958-b1dc96e5b24f?q=80&w=1502&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Placeholder
+      image: "/profilePlaceholder.jpg", // Placeholder
     },
     {
       id: 2,
       name: "Dr. Oikeh Ojeamen",
       title: "Visiting Consultant Plastic Surgeon",
       qualifications: "FWACS, MBBS",
-      image:
-        "https://images.unsplash.com/photo-1673295716958-b1dc96e5b24f?q=80&w=1502&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Placeholder
+      image: "/profilePlaceholder.jpg", // Placeholder
     },
     {
       id: 3,
       name: "Dr. Dades Obed",
       title: "Visiting General and Laparoscopic Surgeon",
       qualifications: "MBBS, FMCS, FMAS, FICS",
-      image:
-        "https://images.unsplash.com/photo-1673295716958-b1dc96e5b24f?q=80&w=1502&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Placeholder
+      image: "/profilePlaceholder.jpg", // Placeholder
     },
   ];
 
